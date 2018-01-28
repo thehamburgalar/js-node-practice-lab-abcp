5bc7bf98fcc1ab9abb3b8ba8a652ef66a0e78bd3
Space Captin <thehamburgalar1@gmail.com>
2018-01-28 23:22:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Space"
-var height = "76"
+var height = 76
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) +1)
+console.log(height +1)
